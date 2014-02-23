f5810e7c1985b635fa54c4e88b3581ad4d2a34d5
davelldf <davelldf@rose-hulman.edu>
2014-02-23 01:00:45 -0500

    formats

--- video_maker.m
+++ video_maker.m
@@ -7,7 +7,7 @@ fileList = dir(directory);
 for i = 3+start:finish
     filename = [directory '\' num2str(i-2) '.png'];
     img = imread(filename);
-   writeVideo(writer, im2frame(img));
+    writeVideo(writer, im2frame(img));
 end
 close(writer);
 end
